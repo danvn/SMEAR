2728c3396473bb4621f653f7b98b92568a6d7ed6
danvn <dan.nguyen7@yahoo.com>
2018-04-29 15:17:37 -0600

    GetPlayerFollowers

---
+++ getPlayerFollowers.py
@@ -0,0 +1,10 @@
+from instagram.client import InstagramAPI
+
+access_token = "23610210.d225dd2.ba7559939d0b4feba16b548f1960bd16"
+client_secret = "3f0edbecfa6e405593123af948d249c8"
+api = InstagramAPI(access_token=access_token, client_secret=client_secret)
+# recent_media, next_ = api.user_recent_media(user_id="danvn", count=10)
+recent_media= api.user_recent_media(user_id="danvn", count=10)
+
+# for media in recent_media:
+#    print (media.caption.text)
