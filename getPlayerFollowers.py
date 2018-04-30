7520072028686843cd3f3c7f390f1ab9fde745c1
Dan Nguyen <dang0146@colorado.edu>
2018-04-30 17:58:32 -0600

    Update getPlayerFollowers.py

--- getPlayerFollowers.py
+++ getPlayerFollowers.py
@@ -3,7 +3,7 @@ from instagram.client import InstagramAPI
 access_token = ""
 client_secret = ""
 api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-# recent_media, next_ = api.user_recent_media(user_id="danvn", count=10)
+# recent_media, next_ = api.user_recent_media(user_id="", count=10)
 recent_media= api.user_recent_media(user_id="danvn", count=10)
 
 # for media in recent_media:
