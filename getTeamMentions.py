464229fdd3539e3eea28e960823779fa2bb208e0
danvn <dan.nguyen7@yahoo.com>
2018-05-02 09:53:44 -0600

    Able to grab single team, paginate json requests and combine into one json file for team counts daily mentions for the last season include pre and post season

--- getTeamMentions.py
+++ getTeamMentions.py
@@ -1,15 +1,33 @@
 import requests
 from requests.auth import HTTPBasicAuth
+import json as json
 
-url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod/counts.json?query=gnip"
+url = ""
 bucket = "&bucket=day"
 user = ""
-passWord = ""
-
+password = ""
+# fromDate ="201702220000" 	#yyyymmddhhmm
+# toDate = "201711030000"		#yyyymmddhhmm
+timeframe = "&fromDate=201702220000&toDate=201711030000"
 teams = ["@athletics", "@Braves", "@Dodgers", "@Royals", "@tigers", "@Dbacks", "@Reds", "@Padres", "@Pirates", "@Mets", "@Orioles", "@Angels", "@Twins", "@Nationals", "@RaysBaseball", "@RedSox", "@Yankees", "@Rangers", "@Cubs", "@BlueJays", "@Mariners", "@SFGiants", "@astros", "@Brewers", "@Cardinals", "@Indians", "@Marlins", "@Phillies", "@whitesox", "@Rockies"]
-print(len(teams))
-# r = requests.get(url, auth=HTTPBasicAuth(user, passWord))
 
-# r = requests.get('https://api.github.com/events')
+queryParameters = {
+    'bucket': 'day'
+    # 'next': 'eyJhdXRoZW50aWNpdHkiOiJiNDlhMzMxNDExYTljYzBmODE5YmE3YTE3YTYzOGYyMTEyYTgxYTYxZWFkMDkzNjU5NWYxYTY5MjUwMGRmZjljIiwiZnJvbURhdGUiOiIyMDE3MDIyMjAwMDAiLCJ0b0RhdGUiOiIyMDE3MTEwMzAwMDAiLCJidWNrZXQiOiJkYXkiLCJuZXh0Ijp7Im1heERhdGUiOiIyMDE3MTAwMzAwMDAwMCIsImV4cGVuc2l2ZVF1ZXJ5IjpmYWxzZX19'
+    }
 
-# print(r.content)
+r = requests.get(url + teams[0] + timeframe, params=queryParameters, auth=HTTPBasicAuth(user, password))
+
+totalCounts = r.json()
+currentPage = r.json()
+while("next" in currentPage):
+    nextToken = currentPage["next"]
+    queryParameters.update({"next": nextToken})
+    r = requests.get(url + teams[0] + timeframe, params=queryParameters, auth=HTTPBasicAuth(user, password))
+    nextPage = r.json()
+    for day in nextPage["results"]:
+        totalCounts["results"].append(day)
+    currentPage = nextPage
+
+with open(teams[0]+'.json', 'w') as fp:
+    json.dump(totalCounts, fp)
