88c5a5b4d133e157d328d33aade740cfb296497b
danvn <dan.nguyen7@yahoo.com>
2018-05-06 18:01:20 -0600

    got all the team name abbreviations. why do they randomly add a N to everything

--- getTeamMentions.py
+++ getTeamMentions.py
@@ -11,23 +11,22 @@ password = ""
 timeframe = "&fromDate=201702220000&toDate=201711030000"
 teams = ["@athletics", "@Braves", "@Dodgers", "@Royals", "@tigers", "@Dbacks", "@Reds", "@Padres", "@Pirates", "@Mets", "@Orioles", "@Angels", "@Twins", "@Nationals", "@RaysBaseball", "@RedSox", "@Yankees", "@Rangers", "@Cubs", "@BlueJays", "@Mariners", "@SFGiants", "@astros", "@Brewers", "@Cardinals", "@Indians", "@Marlins", "@Phillies", "@whitesox", "@Rockies"]
 
-queryParameters = {
-    'bucket': 'day'
-    # 'next': 'eyJhdXRoZW50aWNpdHkiOiJiNDlhMzMxNDExYTljYzBmODE5YmE3YTE3YTYzOGYyMTEyYTgxYTYxZWFkMDkzNjU5NWYxYTY5MjUwMGRmZjljIiwiZnJvbURhdGUiOiIyMDE3MDIyMjAwMDAiLCJ0b0RhdGUiOiIyMDE3MTEwMzAwMDAiLCJidWNrZXQiOiJkYXkiLCJuZXh0Ijp7Im1heERhdGUiOiIyMDE3MTAwMzAwMDAwMCIsImV4cGVuc2l2ZVF1ZXJ5IjpmYWxzZX19'
-    }
+for team in teams:
+    queryParameters = {
+        'bucket': 'day'
+        }
+    r = requests.get(url + team + timeframe, params=queryParameters, auth=HTTPBasicAuth(user, password))
+    totalCounts = r.json()
+    currentPage = r.json()
 
-r = requests.get(url + teams[0] + timeframe, params=queryParameters, auth=HTTPBasicAuth(user, password))
+    while("next" in currentPage):
+        nextToken = currentPage["next"]
+        queryParameters.update({"next": nextToken})
+        r = requests.get(url + team + timeframe, params=queryParameters, auth=HTTPBasicAuth(user, password))
+        nextPage = r.json()
+        for day in nextPage["results"]:
+            totalCounts["results"].append(day)
+        currentPage = nextPage
 
-totalCounts = r.json()
-currentPage = r.json()
-while("next" in currentPage):
-    nextToken = currentPage["next"]
-    queryParameters.update({"next": nextToken})
-    r = requests.get(url + teams[0] + timeframe, params=queryParameters, auth=HTTPBasicAuth(user, password))
-    nextPage = r.json()
-    for day in nextPage["results"]:
-        totalCounts["results"].append(day)
-    currentPage = nextPage
-
-with open(teams[0]+'.json', 'w') as fp:
-    json.dump(totalCounts, fp)
+    with open(team + '.json', 'w') as fp:
+        json.dump(totalCounts, fp)
